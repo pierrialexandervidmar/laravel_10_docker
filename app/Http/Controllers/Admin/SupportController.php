58207a3c21825c3a8f1099e65809a9975d245c55
pierrialexandervidmar <pierri@hotmail.com.br>
2024-02-26 23:07:17 -0300

    request validator e views

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUpdateSupport;
 use App\Models\Support;
 use Illuminate\Http\Request;
 
@@ -17,11 +18,14 @@ class SupportController extends Controller
 
     public function show(string|int $id)
     {
+        // Support::where('id', $id)->first();
+        // Support::where('id', '!=', $id)->first();
         if (!$support = Support::find($id))
         {
             return redirect()->back();
         }
-        dd($support->subject);
+
+        return view('admin/supports/show', compact('support'));
     }
 
     // Action para chamar a View do formulário de criação.
@@ -30,13 +34,44 @@ class SupportController extends Controller
         return view('admin/supports/create');
     }
 
-    public function store(Request $request, Support $support)
+    public function store(StoreUpdateSupport $request, Support $support)
     {
         $data = $request->all();
         $data['status'] = 'a';
 
         $support->create($data);
 
+        return redirect()->route('supports.index');
+    }
+
+    public function edit(Support $support, string|int $id)
+    {
+        if (!$support = $support->where('id', $id)->first())
+        {
+            return redirect()->back();
+        }
+        return view('admin/supports.edit', compact('support'));
+    }
+
+    public function update(StoreUpdateSupport $request, Support $support, string $id)
+    {
+        if (!$support = $support->where('id', $id)->first())
+        {
+            return redirect()->back();
+        }
+        $support->update($request->only([
+            'subject', 'body'
+        ]));
+        return redirect()->route('supports.index');
+    }
+
+    public function destroy(Support $support, string|int $id)
+    {
+        if (!$support = $support->where('id', $id)->first())
+        {
+            return redirect()->back();
+        }
+        $support->delete();
         return redirect()->route('supports.index');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::delete('/supports/{id}', [SupportController::class, 'destroy'])->name('supports.destroy');
+Route::put('/supports/{id}', [SupportController::class, 'update'])->name('supports.update');
+Route::get('/supports/{id}/edit', [SupportController::class, 'edit'])->name('supports.edit');
 Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
 Route::get('/supports/{id}', [SupportController::class, 'show'])->name('supports.show');
 Route::post('/supports', [SupportController::class, 'store'])->name('supports.store');
